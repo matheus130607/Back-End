e503f53552e7ec4a25ba658d3af7173f74688d10
matheus130607 <matheusmalaman2014@gmail.com>
2025-08-13 14:25:06 -0300

    Entregue

--- Aula 03/Atv10.php
+++ Aula 03/Atv10.php
@@ -9,9 +9,9 @@ Mostre no console:
 • O programa só termina quando o usuário escolher "3 - Sair".
 • Use for para repetir o menu um número fixo de vezes, por exemplo, 5 vezes. */
 
-$final = 5;
 
-for ($i = 1;$i  <= $final; $i++) {
+
+for ($i = 1;$i  <= 5; $i++) {
     
 $opcao = readline("Escolha uma opção:\n 1 - Olá\n 2 - Data Atual\n 3 - Sair");
 switch($opcao) {
